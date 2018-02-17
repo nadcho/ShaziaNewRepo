7c6fc42af3f9b13b9519e9351f2ca79fbbd1423c
NADIM <nadcho@yahoo.com>
2018-02-17 19:11:17 +0000

    adding files

---
+++ LearningEncapsulation/src/GoodDog.java
@@ -0,0 +1,39 @@
+/**
+ * Created by nadimchouglay on 19/12/2016.
+ */
+public class GoodDog {
+    private int size;
+
+    public int getSize(){
+        return size;
+    }
+
+    public void setSize(int s) {
+        size=s;
+    }
+
+    void bark(){
+        if (size>60){
+            System.out.println("woof! woof!");
+        }
+        else if(size>14){
+            System.out.println("Ruff! Ruff!");
+        }
+        else{
+            System.out.println("Yip! Yip!");
+        }
+    }
+}
+
+    public static void main(String[] args) {
+        GoodDog one=new GoodDog();
+        one.setSize(70);
+
+        GoodDog two=new GoodDog();
+        two.setSize(8);
+
+        System.out.println("Dog one: "+one.getSize());
+        System.out.println("Dog two: "+two.getSize());
+        one.bark();
+        two.bark();
+    }
